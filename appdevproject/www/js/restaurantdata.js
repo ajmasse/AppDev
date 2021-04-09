2d72535ef3ad3e6dbdb21efc95ccdd993d6f8765
ajmasse <75096587+ajmasse@users.noreply.github.com>
2021-04-09 14:02:04 -0700

    Add files via upload

---
+++ appdevproject/www/js/restaurantdata.js
@@ -0,0 +1,86 @@
+// RESTAURANT INFO/DESCRIPTIONS
+var restaurantSet = [
+    {
+        id: 0,
+        name: 'The Gourmet Kitchen', 
+        active: true, 
+        keyImage:'gk-1.jpg', 
+        otherImage: [
+            'gk-1.jpg',
+            'gk-2.jpg',
+            'gk-3.jpg',
+            'gk-4.jpg',
+            'gk-5.jpg',
+            'gk-6.jpg',
+        ],
+        
+        shortDesc: 'The Gourmet Kitchen connects locals through great tasting food while also supporting local farmers and utilizing the freshest ingredients.',
+        
+        longDesc: '<p>The Gourmet Kitchen chef and owner Peter Smith, has traveled to over forty countries on five continents, and worked under six different Master Chefs, from France to Asia, at restaurants such as Raymond Blancs Le Manior Aux Quat Saisons, La Contea in Piedmonte, and the legendary Le Cirque in Manhattan. Chef Peter first achieved notoriety in Singapore, where he was nominated as the rising star of Asia. At The Gourmet Kitchen, Chef Peter has created a vivid ambiance at the vanguard of elegant and contemporary gastronomy in Vancouver.</p>',
+        
+        dietaryList: [
+            'Vegan',
+            'Vegetarian',
+            'Keto',
+            'Gluten-free',
+            'Lactose-free',
+            'Organic',
+        ]    
+    },
+    
+    {id: 1, name: 'Basic Kneads Pizza', 
+            active: false, 
+            keyImage:'rest-1.jpg', 
+            shortDesc: 'Morbi enim nunc faucibus a. Pretium vulputate sapien nec sagittis.'},
+
+    {id: 2, name: 'The Dairy Godmother', 
+            active: false, 
+            keyImage:'rest-2.jpg', 
+            shortDesc: 'Risus sed vulputate odio ut enim blandit volutpat maecenas.'},
+    
+    {id: 3, name: 'Lord of the Fries', 
+            active: false, 
+            keyImage:'rest-3.jpg', 
+            shortDesc: 'Fermentum leo vel orci porta non. Odio ut sem nulla pharetra diam sit amet nisl suscipit.'},
+   
+    {id: 4, name: 'Indulge', 
+            active: false, 
+            keyImage:'rest-4.jpg', 
+            shortDesc: 'Mi proin sed libero enim sed faucibus turpis.'},
+    
+    {id: 5, name: 'Pink Sugar', 
+            active: false, 
+            keyImage:'rest-5.jpg', 
+            shortDesc: 'Proin sagittis nisl rhoncus mattis rhoncus urna neque viverra justo.'},
+    
+    {id: 6, name: 'Banana Leaf', 
+            active: false, 
+            keyImage:'rest-6.jpg', 
+            shortDesc: 'Nulla aliquet porttitor lacus luctus accumsan tortor posuere ac.'},
+    
+    {id: 7, name: 'The Codfather', 
+            active: false, 
+            keyImage:'rest-7.jpg', 
+            shortDesc: 'Sed libero enim sed faucibus turpis in eu.'},
+    
+    {id: 8, name: 'Chez Panisse Cafe', 
+            active: false, 
+            keyImage:'rest-8.jpg', 
+            shortDesc: 'Imperdiet sed euismod nisi porta lorem mollis aliquam ut.'},
+    
+    {id: 9, name: 'Leaping Lizard Cafe ', 
+            active: false, 
+            keyImage:'rest-9.jpg', 
+            shortDesc: 'Diam vel quam elementum pulvinar etiam non quam lacus.'},
+    
+    {id: 10,name: 'The Glass Onion', 
+            active: false, 
+            keyImage:'rest-10.jpg', 
+            shortDesc: 'Sollicitudin nibh sit amet commodo nulla facilisi nullam.'},
+    
+    {id: 11,name: 'The Saffron Boutique', 
+            active: false, 
+            keyImage:'rest-11.jpg', 
+            shortDesc: 'Erat velit scelerisque in dictum non consectetur a erat nam.'}
+
+];
